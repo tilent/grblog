e6cc1ef6d52198ed48a700ec4dba29168af5c694
ChanceDoor <cd19900419@hotmail.com>
2013-07-20 04:44:37 -0400

    blog

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '28b5b01b88a371afc17fe8d30ff7253fc4cbb1782de8e2d39e2d5b7b8933a9250f3a1207c7c3943029abc764d38702ddea71af60b54b4912a638564f6e9d2758'
