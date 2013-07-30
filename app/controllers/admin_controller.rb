12efaa126b693a1f33d49229c770262375f548a3
ChanceDoor <cd19900419@hotmail.com>
2013-07-30 04:42:04 -0400

    分离管理后台

---
+++ app/controllers/admin_controller.rb
@@ -0,0 +1,5 @@
+class AdminController < ApplicationController
+  def index
+     @posts = Post.all
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,10 @@ Blog::Application.routes.draw do
   resources :posts do
     resources :comments
   end
+
+  scope '/admin' do
+    resources :admin_posts
+  end
+  get 'admin'  => 'admin#index'
   root :to => 'posts#index'
 end
